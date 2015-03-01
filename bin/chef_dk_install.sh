d3dc9bb44d9d8ba1c2a7913269bd29298750e44d
Brandon Burton <brandon@inatree.org>
2015-02-28 19:45:04 -0500

    testing

--- bin/chef_dk_install.sh
+++ bin/chef_dk_install.sh
@@ -15,9 +15,9 @@ install_chefdk() {
   CHEF_DK_VERSION="0.4.0-1"
   CHEF_DK_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_"$CHEF_DK_VERSION"_amd64.deb
   CHEF_DK_DEB=/tmp/chefdk_"$CHEF_DK_VERSION"_installer.deb
-  #curl -s $CHEF_DK_URL -o $CHEF_DK_DEB
-  sudo dpkg -i $CHEF_DK_DEB > /dev/null 2>&1
-  chef verify > /dev/null 2>&1
+  curl -s $CHEF_DK_URL -o $CHEF_DK_DEB
+  sudo dpkg -i $CHEF_DK_DEB # > /dev/null 2>&1
+  chef verify # > /dev/null 2>&1
   chefdk_installed_correctly=$?
   if [[ $chefdk_installed_correctly != 0 ]]; then
     echo "ChefDK not installed correctly, failing"
