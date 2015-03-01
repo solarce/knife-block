c2ba2d2da940a1d19421116cac0a87eb21ec726e
Brandon Burton <brandon@inatree.org>
2015-02-28 23:14:23 -0500

    testing

--- bin/chef_dk_install.sh
+++ bin/chef_dk_install.sh
@@ -12,12 +12,18 @@ ubuntu_check(){
 }
 
 install_chefdk() {
-  CHEF_DK_VERSION="0.4.0-1"
-  CHEF_DK_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_"$CHEF_DK_VERSION"_amd64.deb
-  CHEF_DK_DEB=/tmp/chefdk_"$CHEF_DK_VERSION"_installer.deb
+  #CHEF_DK_VERSION="0.4.0-1"
+  #CHEF_DK_URL=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_"$CHEF_DK_VERSION"_amd64.deb
+  #CHEF_DK_DEB=/tmp/chefdk_"$CHEF_DK_VERSION"_installer.deb
   ubuntu_check
-  curl -s $CHEF_DK_URL -o $CHEF_DK_DEB
-  sudo dpkg -i $CHEF_DK_DEB > /dev/null 2>&1
+  #curl -s $CHEF_DK_URL -o $CHEF_DK_DEB
+  #sudo dpkg -i $CHEF_DK_DEB > /dev/null 2>&1
+  curl https://packagecloud.io/gpg.key | sudo apt-key add -
+  echo "deb https://packagecloud.io/chef/stable/ubuntu/ precise main" \
+    | sudo tee -a /etc/apt/sources.list.d/chef.list
+  sudo apt-get install -y apt-transport-https
+  sudo apt-get update -qq
+  sudo apt-get install -y chefdk
   chef verify > /dev/null 2>&1
   chefdk_installed_correctly=$?
   if [[ $chefdk_installed_correctly != 0 ]]; then
